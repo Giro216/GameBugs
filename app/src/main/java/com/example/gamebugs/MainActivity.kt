94b7b9c930d81eed35b09d025e6dc288bd4f40e1
Giro216 <maks07072004@gmail.com>
2025-09-10 13:15:57 +0700

    Init repository

---
+++ app/src/main/java/com/example/gamebugs/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gamebugs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gamebugs.ui.components.MainActivityGreeting
+import com.example.gamebugs.ui.theme.GameBugsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GameBugsTheme {
+                MainActivityGreeting(
+                    name = "World"
+                )
+            }
+        }
+    }
+}
