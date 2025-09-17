c65fe4b5e0c9791e0fdcbaa20e5ce550145f332f
Giro216 <maks07072004@gmail.com>
2025-09-17 11:53:28 +0700

    Init LAB2

--- app/src/main/java/com/example/gamebugs/MainActivity.kt
+++ app/src/main/java/com/example/gamebugs/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.gamebugs.ui.components.MainActivityGreeting
+import androidx.compose.ui.node.DelegatableNode
+import androidx.compose.ui.res.stringResource
+import com.example.gamebugs.ui.components.GreetingImage
+import com.example.gamebugs.ui.components.GreetingText
+import com.example.gamebugs.ui.components.RegistrationPanel
 import com.example.gamebugs.ui.theme.GameBugsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +21,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GameBugsTheme {
-                MainActivityGreeting(
-                    name = "World"
-                )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    RegistrationPanel()
+                }
+
+//                Surface (
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    GreetingArticle()
+//                }
+
             }
         }
     }
